1afca1dbf57a996ced36ce599cfc0df971e3c1ae
pro585code <pro585g@gmail.com>
2015-10-21 14:24:01 -0400

    finishedcode

--- Lab07/LightShowROM.vhd
+++ Lab07/LightShowROM.vhd
@@ -60,6 +60,7 @@ architecture Behavioral of LightShowROM is
           x"1"  & "0000000000001111", -- Delay 31 ms (approx 1/64 sec)
 			 
 			 x"c"	 & "0000000000100010", -- Branch 34
+			 x"9"  & "0000000000111000", -- Branch 52
 			 x"8"  & "0000000000000000", -- Jump 0
 			 
 			 x"4"  & "1111111001111111", -- Load LED
@@ -79,10 +80,39 @@ architecture Behavioral of LightShowROM is
           x"4"  & "0000000110000000", -- Load LED
           x"1"  & "0000000000011111", -- Delay x ms (approx 1/8 sec)
 			 
-			 x"a"  & "0000000000000000",  -- Branch 0 
-          x"8"  & "0000000000100010",  -- Jump 34
-          
+			 x"a"  & "0000000000000000", -- Branch 0
+			 x"9"  & "0000000000111000", -- Branch 52
+          x"8"  & "0000000000100010", -- Jump 34
+          	 
+		    x"4"  & "1010100000010101", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "0110100000010110", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "0101100000011010", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "0101010000101010", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "0101001001001010", -- Load LED
+	       x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "0101000110001010", -- Load LED
+	       x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+		    x"4"  & "1101000110001011", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+          x"4"  & "1011001001001101", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+		    x"4"  & "1010110000110101", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+		    x"4"  & "1011100000011101", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+		    x"4"  & "1101100000011011", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
+		    x"4"  & "1110100000010111", -- Load LED
+		    x"1"  & "0000000001111111", -- Delay 127 ms (approx 1/8 sec)
 			 
+		    x"a"  & "0000000000000000", -- Branch 0
+          x"c"  & "0000000000000000", -- Branch 34		  
+          x"8"  & "0000000000111000", -- Jump 52
+      
           others => (others => '0')
 	);
    
